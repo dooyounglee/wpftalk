6ad0495fef02f5ed10a84d4a10b82f50cb574de4
doo <gostbaducking1@nate.com>
2025-03-24 23:50:52 +0900

    feat: Room 추가

---
+++ Models/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace talk2.Models
+{
+    public class Room
+    {
+        public int RoomNo { get; set; }
+        public string Title { get; set; }
+
+        public Room(int RoomNo, string Title)
+        {
+            this.RoomNo = RoomNo;
+            this.Title = Title;
+        }
+    }
+}
