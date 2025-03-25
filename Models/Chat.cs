fdf2fe35a31a1fccfc7dcb69bfd813fdc812e295
doo <gostbaducking1@nate.com>
2025-03-26 00:30:57 +0900

    feat: Chat, User 추가

---
+++ Models/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace talk2.Models
+{
+    public class Chat
+    {
+        public int ChatNo { get; set; }
+        public int UsrNo { get; set; }
+        public string chat { get; set; }
+        public string Align { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace talk2.Models
+{
+    class User
+    {
+        public int UsrNo { get; set; }
+        public string UsrId { get; set; }
+        public string UsrNm { get; set; }
+        public byte[]? Password { get; set; }
+        public string? Password1 { get; set; }
+        public int DivNo { get; set; }
+        public string DivNm { get; set; }
+        public string Ip { get; set; }
+        public int Port { get; set; }
+    }
+}
