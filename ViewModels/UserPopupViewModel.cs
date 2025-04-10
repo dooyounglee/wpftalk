f2b7146a4a6dd83765b6bfafe94f52733dfdd279
doo <gostbaducking1@nate.com>
2025-04-11 00:23:15 +0900

    feat: 사용자선택 팝업 추가

---
+++ ViewModels/UserPopupViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using talk2.Commands;
+using talk2.Models;
+using talk2.Services;
+
+namespace talk2.ViewModels
+{
+    public class UserPopupViewModel
+    {
+        private readonly IUserService _userService;
+        public UserPopupViewModel(IUserService userService)
+        {
+            _userService = userService;
+
+            _userList = _userService.getUserList();
+        }
+
+        private List<User> _userList;
+        public List<User> UserList { get => _userList; }
+    }
+}
