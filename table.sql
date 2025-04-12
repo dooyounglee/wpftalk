732867eb2855002ada3cb7565466771cd2a79c83
doo <gostbaducking1@nate.com>
2025-04-12 22:27:47 +0900

    fix: pk추가, chatuser.chat_no 컬럼 추가

--- table.sql
+++ table.sql
@@ -3,20 +3,21 @@
 -- DROP SCHEMA talk;
 CREATE SCHEMA talk AUTHORIZATION postgres;
 
--- DROP TABLE talk."USER";
-CREATE TABLE talk."USER" (
+-- DROP TABLE talk."user";
+CREATE TABLE talk."user" (
 	usr_no int8 NOT NULL,
 	usr_nm varchar NOT NULL,
 	div_no int8 NULL,
 	usr_id varchar NULL,
 	usr_pw varchar NULL,
-	CONSTRAINT usr_pk PRIMARY KEY (usr_no)
+	CONSTRAINT user_pk PRIMARY KEY (usr_no)
 );
 
 -- DROP TABLE talk.div;
 CREATE TABLE talk.div (
 	div_no int8 NOT NULL,
-	div_nm varchar NOT NULL
+	div_nm varchar NOT NULL,
+	CONSTRAINT div_pk PRIMARY KEY (div_no)
 );
 
 -- DROP TABLE talk.chat;
@@ -26,14 +27,17 @@ CREATE TABLE talk.chat (
 	chat_fg varchar NOT NULL,
 	room_no int8 NOT NULL,
 	usr_no int8 NOT NULL,
-	rgt_dtm varchar NULL
+	rgt_dtm varchar NULL,
+	CONSTRAINT chat_pk PRIMARY KEY (chat_no)
 );
 
 -- DROP TABLE talk.chatuser;
 CREATE TABLE talk.chatuser (
 	room_no int8 NOT NULL,
 	usr_no int8 NOT NULL,
-	title varchar NOT NULL
+	title varchar NOT NULL,
+	chat_no int8,
+	CONSTRAINT chatuser_pk PRIMARY KEY (room_no,usr_no)
 );
 
 -- DROP TABLE talk.room;
@@ -41,5 +45,6 @@ CREATE TABLE talk.room (
 	room_no int8 NOT NULL,
 	usr_no int8 NOT NULL,
 	title varchar NOT NULL,
-	rgt_dtm varchar NULL
+	rgt_dtm varchar NULL,
+	CONSTRAINT room_pk PRIMARY KEY (room_no)
 );
