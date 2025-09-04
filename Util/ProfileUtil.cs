3a8bb20855d0f59e3b6e3b9c35a4c7551e70c817
doo <gostbaducking1@nate.com>
2025-09-04 22:49:15 +0900

    temp: 프로필사진 가져오는 util추가(임시)

---
+++ Util/ProfileUtil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace talkLib.Util
+{
+    class ProfileUtil
+    {
+        private static readonly Dictionary<int, BitmapImage> _cache = new Dictionary<int, BitmapImage>();
+
+        public static async Task<BitmapImage> GetProfileImageAsync(int usrNo)
+        {
+            if (_cache.TryGetValue(usrNo, out var cachedImage))
+                return cachedImage;
+
+            try
+            {
+                var bitmap = new BitmapImage(ImageUtil.getProfile(usrNo));
+                _cache[usrNo] = bitmap;
+                return bitmap;
+            }
+            catch
+            {
+                // 실패시 null이나 기본 이미지 리턴 처리
+                return null;
+            }
+        }
+    }
+}
