07926f48d675ac66e2b10edeb18cdbdcedd0f255
doo <gostbaducking1@nate.com>
2025-09-06 00:48:47 +0900

    fix: IsMine으로 템플릿selector 분기

---
+++ TemplateSelector/ChatMessageTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using talk2.Models;
+
+namespace talk2.Views
+{
+    public class ChatMessageTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate MyMessageTemplate { get; set; }
+        public DataTemplate OtherMessageTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var message = item as Chat;
+            if (message == null)
+                return base.SelectTemplate(item, container);
+
+            return message.IsMine ? MyMessageTemplate : OtherMessageTemplate;
+        }
+    }
+
+}
