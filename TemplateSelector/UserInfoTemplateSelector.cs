e43c570638b4a9baac860b749bccddcf471a8d5f
doo <gostbaducking1@nate.com>
2025-10-26 01:48:18 +0900

    design: 관리자용, 일반사용자용 화면 분리

---
+++ TemplateSelector/UserInfoTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using talk2.ViewModels;
+
+namespace talk2.Views
+{
+    class UserInfoTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate AdminTemplate { get; set; }
+        public DataTemplate UserTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is NewUserViewModel vm)
+                return vm.IsNotAdmin ? UserTemplate : AdminTemplate;
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
